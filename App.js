18a96fd3fbc9030883d82833c5e45bedf5f6b11f
Kularasa Sanjeevan <1997sanjee@gmail.com>
2021-10-10 12:38:30 +0530

    describtion

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <GlobalProvider>
      <Provider store={store}>
-      <AppContainer />
+       <AppContainer />
      </Provider>    
     </GlobalProvider>
 
