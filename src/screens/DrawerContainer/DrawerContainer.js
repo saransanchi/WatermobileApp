03bf5291089b7ae35f6251b56599900ed77630db
Kularasa Sanjeevan <1997sanjee@gmail.com>
2021-10-10 20:40:33 +0530

    drawer design

--- src/screens/DrawerContainer/DrawerContainer.js
+++ src/screens/DrawerContainer/DrawerContainer.js
@@ -1,98 +1,67 @@
 import React from "react";
-import { View,Text ,TouchableOpacity,Image} from "react-native";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 import PropTypes from "prop-types";
 import styles from "./styles";
 import MenuButton from "../../components/MenuButton/MenuButton";
-import  firebase  from "../../firebase";
+import firebase from "../../firebase";
+import { GlobalContext } from "../../context/GlobalContext";
 
 
+const image = { uri: "http://gsmcloud.xyz/profile.png" };
 export default class DrawerContainer extends React.Component {
+
+  static contextType = GlobalContext
+
+  componentDidMount() {
+    const contextObj = this.context
+
+    console.log(contextObj.authUser) // { name: 'Tania', loggedIn: true }
+  }
+
   render() {
     const { navigation } = this.props;
     return (
-       <View style={styles.drawer}>
-        
-          {/* <View style={styles.accountcontent}>
-           <Text>hiiii</Text>
-          </View>   */}
-        {/* <View style={styles.content}>
-          </View>
-        <View style={styles.container}>
-          <MenuButton
-            title="Home"
-            source={require("../../../assets/icons/home.png")}
+      <View style={styles.drawer}>
+        <View style={styles.accountcontent}>
+          <Image source={image} style={{ height: 80, width: 80, marginLeft: 20, marginTop: 40 }} />
+          <Text style={{ color: "white", fontSize: 17, fontWeight: "bold", marginLeft: 20, marginTop: 30 }}>sarn sanchi</Text>
+        </View>
+        <View style={{ marginTop: 20, marginLeft: 15 }}>
+          <TouchableOpacity style={{ flexDirection: "row", marginTop: 10 }}
             onPress={() => {
               navigation.navigate("Home");
               navigation.closeDrawer();
-            }}
-          />
-          <MenuButton
-            title="Orders"
-            source={require("../../../assets/icons/category.png")}
+            }}>
+            <Image source={require("../../../assets/icons/home.png")} style={{ height: 20, width: 20 }} />
+            <Text style={{ color: "black", fontSize: 17, fontWeight: "bold", marginLeft: 15 }}>Home</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ flexDirection: "row", marginTop: 10 }}
             onPress={() => {
-              navigation.navigate("Categories");
+              navigation.navigate("Home");
               navigation.closeDrawer();
-            }}
-          />
-          <MenuButton
-            title="SEARCH"
-            source={require("../../../assets/icons/search.png")}
-            onPress={() => {
-              navigation.navigate("Search");
-              navigation.closeDrawer();
-            }}
-          />
-          <MenuButton
-            title="LOGOUT"
-            onPress={() => {
-              firebase.auth()
-                .signOut()
-                .then(() => {
-                  navigation.navigate("SignIn");
-                })
-                .catch((error) => alert(error.message));
-            }}
-          /> */}
-          <View style={styles.accountcontent}>
-          <Text style={{color:"black",fontSize:17,fontWeight:"bold",marginLeft:15}}>Home</Text>
-
-
+            }}>
+            <Image source={require("../../../assets/icons/category.png")} style={{ height: 20, width: 20 }} />
+            <Text style={{ color: "black", fontSize: 17, fontWeight: "bold", marginLeft: 15 }}>Orders</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ flexDirection: "row", marginTop: 10 }}>
+            <Image source={require("../../../assets/icons/info.png")} style={{ height: 20, width: 20 }} />
+            <Text style={{ color: "black", fontSize: 17, fontWeight: "bold", marginLeft: 15 }}>Account</Text>
+          </TouchableOpacity>
+          <View style={{ width: 150, backgroundColor: "blue", marginTop: 200, borderRadius: 15, paddingLeft: 25, paddingBottom:10 }}>
+            <TouchableOpacity style={{ flexDirection: "row", marginTop: 10 }}
+              onPress={() => {
+                firebase.auth()
+                  .signOut()
+                  .then(() => {
+                    navigation.navigate("SignIn");
+                  })
+                  .catch((error) => alert(error.message));
+              }}>
+              <Text style={{ color: "white", fontSize: 16, fontWeight: "bold", marginLeft: 20 }}>LOGOUT</Text>
+            </TouchableOpacity>
           </View>
-          <View style={{marginTop:40, marginLeft:15}}>
-          <TouchableOpacity style={{flexDirection:"row",marginTop:10}} 
-          onPress={() => {
-            navigation.navigate("Home");
-            navigation.closeDrawer();
-          }}>
-          <Image source={require("../../../assets/icons/home.png")} style={{height: 20, width: 20}} />
-            <Text style={{color:"black",fontSize:17,fontWeight:"bold",marginLeft:15}}>Home</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{flexDirection:"row",marginTop:10}}
-          onPress={() => {
-            navigation.navigate("Home");
-            navigation.closeDrawer();
-          }}>
-          <Image source={require("../../../assets/icons/category.png")} style={{height: 20, width: 20}} />
-            <Text style={{color:"black",fontSize:17,fontWeight:"bold",marginLeft:15}}>Orders</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{flexDirection:"row",marginTop:10}}>
-          <Image source={require("../../../assets/icons/home.png")} style={{height: 20, width: 20}} />
-            <Text style={{color:"black",fontSize:17,fontWeight:"bold",marginLeft:15}}>home</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={{flexDirection:"row",marginTop:10 }}
-          onPress={() => {
-            firebase.auth()
-              .signOut()
-              .then(() => {
-                navigation.navigate("SignIn");
-              })
-              .catch((error) => alert(error.message));}}>
-          <Text style={{color:"black",fontSize:17,fontWeight:"bold",marginLeft:15, backgroundColor:"blue", marginBottom:200}}>LOGOUT</Text>
-          </TouchableOpacity>
-          </View>
-        {/* </View> */}
-      
-    </View>
+        </View>
+      </View>
     );
   }
 }
--- src/screens/DrawerContainer/styles.js
+++ src/screens/DrawerContainer/styles.js
@@ -7,26 +7,10 @@ const styles = StyleSheet.create({
     width:200,
   },
   accountcontent:{
-    backgroundColor:"blue",
-    height:200,
+    backgroundColor:"blue", 
     width:200,
+    height:200
   },
-  content: {
-    flexDirection: 'row',
-    height:200,
-    width:200,
-    // backgroundColor:"black"
-
-  },
-  container: {
-    alignItems:'flex-start',
-    width:"100%",
-    paddingRight:200,
-    backgroundColor:"red",
-    marginTop:200,
-    height:50
-
-  }
 });
 
 export default styles;
