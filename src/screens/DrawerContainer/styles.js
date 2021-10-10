e309324c22568502a9732e6e19a9eaf9f14b2466
Kularasa Sanjeevan <1997sanjee@gmail.com>
2021-10-10 12:04:05 +0530

    side drawer design

--- src/screens/DrawerContainer/styles.js
+++ src/screens/DrawerContainer/styles.js
@@ -1,16 +1,31 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  content: {
+  drawer:{
     flex: 1,
+    height:"100%",
+    width:200,
+  },
+  accountcontent:{
+    backgroundColor:"blue",
+    height:200,
+    width:200,
+  },
+  content: {
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
+    height:200,
+    width:200,
+    // backgroundColor:"black"
+
   },
   container: {
-    flex: 1,
-    alignItems: 'flex-start',
-    paddingHorizontal: 20
+    alignItems:'flex-start',
+    width:"100%",
+    paddingRight:200,
+    backgroundColor:"red",
+    marginTop:200,
+    height:50
+
   }
 });
 
